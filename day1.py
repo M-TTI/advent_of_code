42505ee92f23e28471958170fd33a9cc90aba821
Martin Bonetti <1martinbonetti@gmail.com>
2023-12-01 20:45:37 +0100

    day1 done

--- day1.py
+++ day1.py
@@ -1,11 +1,10 @@
-stringf = ''
+stringf = []
 
 
 def sum_digits(string):
     l = ''
     somme = 0
-    split = string.splitlines()
-    for i in split:
+    for i in string:
         for j in i:
             if j.isnumeric():
                 l += j
@@ -14,16 +13,29 @@ def sum_digits(string):
         else:
             nb = l[0] + l[-1]
         l = ''
+        print(nb)
         somme += int(nb)
     return somme
 
 
 def sum_numbers(string):
-
+    corrected_string = []
+    digits_string = []
+    digits = {"one" : "1", "two" : "2", "three" : "3", "four" : "4", "five" : "5", "six" : "6", "seven" : "7", "eight" : "8", "nine" : "9", "ten" : "10"}
+    errors = {"oneight" : "oneeight", "twone" : "twoone", "treeight": "threeeight", "fiveight": "fiveeight", "sevenine" : "sevennine", "eightwo": "eighttwo", "eighten": "eightten", "nineight": "nineeight", "tenine" : "tennine"}
+    for i in string:
+        for j,k in errors.items():
+            i = i.replace(j,k)
+        corrected_string.append(i)
+    for i in corrected_string:
+        for j,k in digits.items():
+            i = i.replace(j,k)
+        digits_string.append(i)
+    return digits_string
 
 
 with open("day1_data", "r") as file:
     for line in file:
-        stringf += line
+        stringf.append(line)
 
-print(sum_digits(stringf))
+print(sum_digits(sum_numbers(stringf)))
