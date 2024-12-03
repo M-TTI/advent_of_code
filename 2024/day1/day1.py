41193c48dcb3f0c23e2da024ebb6ae761922a5c5
martin.bonetti <1martinbonetti@gmail.com>
2024-12-03 09:39:30 +0100

    2 stars now

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,19 +1,39 @@
 left = []
 right = []
 tot = 0
+sim = 0
 with open('day1_input.txt', 'r') as file:
     for line in file:
         line = line.replace('\n', '')
         a = [int(i) for i in line.split()]
         left.append(a[0])
         right.append(a[1])
+    file.close()
 
+# Part 1
 for i in range(len(left)):
     l = left.index(min(left))
     r = right.index(min(right))
     tot += abs(int(left[l]) - int(right[r]))
-    print(f'{i} : {abs(int(left[l]) - int(right[r]))}')
     left.pop(l)
     right.pop(r)
 
-print(tot)
+print(f'total distance = {tot}')
+
+with open('day1_input.txt', 'r') as file:
+    for line in file:
+        line = line.replace('\n', '')
+        a = [int(i) for i in line.split()]
+        left.append(a[0])
+        right.append(a[1])
+    file.close()
+
+# Part 2
+for i in left:
+    c = 0
+    for j in right:
+        if i == j:
+            c += 1
+    sim += i * c
+
+print(f'similarity score = {sim}')
